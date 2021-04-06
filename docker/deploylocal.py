b14f4343a9dcef2cda96600316ea9ff01afe522f
GR-Tang <9yselle@gmail.com>
2021-04-06 10:19:57 +0800

    updated comments for Dockerfile and deploylocal.py

--- docker/deploylocal.py
+++ docker/deploylocal.py
@@ -1,3 +1,5 @@
+# importing necessary libraries
+
 import uvicorn
 from fastapi import FastAPI, File, UploadFile
 from starlette.responses import RedirectResponse
@@ -9,11 +11,14 @@ from tensorflow.keras.applications.imagenet_utils import decode_predictions
 from tensorflow.keras.models import load_model
 import os
 
-app = FastAPI()
 
+# setting parameters
+
+app = FastAPI()
 input_shape = (224, 224)
 
-# Model is placeholder
+# loading saved model
+
 save_folder = "../models"
 model_name = "effnet1"
 model = load_model(f"{save_folder}/{model_name}")
@@ -22,7 +27,8 @@ def read_image(file):
     image = Image.open(BytesIO(file))
     return image
 
-# Image preprocessing method is placeholder too
+# defining image preprocess 
+
 def preprocess(image: Image.Image):
     image = image.resize(input_shape)
     image = np.asfarray(image)
@@ -30,7 +36,8 @@ def preprocess(image: Image.Image):
     image = np.expand_dims(image, 0)
     return image
 
-# Prediction also placeholder
+# loading the image for inference
+
 def predict(image: np.ndarray):
     pred = model.predict(image)
     if pred[0][0] > pred[0][1]:
@@ -39,7 +46,8 @@ def predict(image: np.ndarray):
         infer = "Without Mask"
     return infer
 
-# Actual API for the inference 
+# Restful API for the inference 
+
 @app.get("/", include_in_schema=False)
 async def index():
     return RedirectResponse(url="/docs")
@@ -52,7 +60,10 @@ async def predict_image(file: UploadFile = File(...)):
     print(infer)
     return infer
 
+# on starting the service
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
-    # change host to "0.0.0.0" before building image to deploy on heroku
+    # change host to "0.0.0.0" before building image to deploy on heroku 
+    # or "127.0.0.1" for local
     uvicorn.run(app, port = port, host = "0.0.0.0")
